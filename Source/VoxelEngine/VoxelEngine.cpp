0e9e800db67829dee6ffdb535c5ce0a2401bcbba
Devashish Lal <laldevashish@gmail.com>
2020-06-10 16:51:52 +0530

    Initial Commit

---
+++ Source/VoxelEngine/VoxelEngine.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VoxelEngine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VoxelEngine, "VoxelEngine" );
